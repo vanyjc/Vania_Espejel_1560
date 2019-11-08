795882cd9a29557f51ba46ddaf12b5ca01faaa04
vanyjc <31527650+vanyjc@users.noreply.github.com>
2019-11-08 14:30:35 -0600

    Add files via upload

---
+++ arboles/arboles_binarios.py
@@ -0,0 +1,7 @@
+class NodoArbol:
+    def __init__(self,value,izquierdo=None,derecho=None):
+        self.data=value
+        self.left=izquierdo
+        self.right=derecho
+
+ ##arboles binarios       
---
+++ arboles/tarea_arbol.py
@@ -0,0 +1,6 @@
+from arboles_binarios import NodoArbol
+
+def main():
+    root=NodoArbol(10,NodoArbol(20,NodoArbol(30,None,NodoArbol(40,NodoArbol(50),NodoArbol(60,None,NodoArbol(70,NodoArbol(80)))))))
+
+main()
